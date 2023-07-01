944e8b100ebb8f4340ed807e8c1866ef5ec29be1
Expo <crdailey01@gmail.com>
2023-06-30 22:10:24 -0700

    practice/notecards

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import com.sun.source.tree.BinaryTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.TreeVisitor;
+import org.w3c.dom.Node;
+
+import javax.swing.tree.TreeNode;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BinarySearchTree test = new BinarySearchTree();
+
+
+    }
+}
+
+// stack overflow, recursive case, base case,
